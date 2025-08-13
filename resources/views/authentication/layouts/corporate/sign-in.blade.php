d0b9be5355f57658caaa839b25e0efa65f7d4cb8
chiaaZee <hinaachaft@gmail.com>
2025-08-13 22:30:49 +0700

    jgndihapus

--- resources/views/authentication/layouts/corporate/sign-in.blade.php
+++ resources/views/authentication/layouts/corporate/sign-in.blade.php
@@ -45,7 +45,7 @@ License: For each use you must have a valid license purchased only from above li
 		<!--end::Theme mode setup on page load-->
 		<!--begin::Root-->
 		<div class="d-flex flex-column flex-root" id="kt_app_root">
-			<!--begin::Authentication - Sign-up -->
+			<!--begin::Authentication - Sign-in -->
 			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
 				<!--begin::Body-->
 				<div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
@@ -54,62 +54,35 @@ License: For each use you must have a valid license purchased only from above li
 						<!--begin::Wrapper-->
 						<div class="w-lg-500px p-10">
 							<!--begin::Form-->
-							<form class="form w-100" novalidate="novalidate" id="kt_sign_up_form" data-kt-redirect-url="authentication/layouts/corporate/sign-in.html" action="#">
+							<form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="{{ route ('index') }}" action="#">
 								<!--begin::Heading-->
 								<div class="text-center mb-11">
 									<!--begin::Title-->
-									<h1 class="text-gray-900 fw-bolder mb-3">Sign Up</h1>
+									<h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
 									<!--end::Title-->
 									<!--begin::Subtitle-->
 									<div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
 									<!--end::Subtitle=-->
 								</div>
+								<!--begin::Heading-->
 								<!--begin::Input group=-->
 								<div class="fv-row mb-8">
 									<!--begin::Email-->
 									<input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
 									<!--end::Email-->
 								</div>
-								<!--begin::Input group-->
-								<div class="fv-row mb-8" data-kt-password-meter="true">
-									<!--begin::Wrapper-->
-									<div class="mb-1">
-										<!--begin::Input wrapper-->
-										<div class="position-relative mb-3">
-											<input class="form-control bg-transparent" type="password" placeholder="Password" name="password" autocomplete="off" />
-											<span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2" data-kt-password-meter-control="visibility">
-												<i class="ki-duotone ki-eye-slash fs-2"></i>
-												<i class="ki-duotone ki-eye fs-2 d-none"></i>
-											</span>
-										</div>
-										<!--end::Input wrapper-->
-										<!--begin::Meter-->
-										<div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
-										</div>
-										<!--end::Meter-->
-									</div>
-									<!--end::Wrapper-->
-									<!--begin::Hint-->
-									<div class="text-muted">Use 8 or more characters with a mix of letters, numbers & symbols.</div>
-									<!--end::Hint-->
-								</div>
 								<!--end::Input group=-->
-								<!--end::Input group=-->
-								<div class="fv-row mb-8">
-									<!--begin::Repeat Password-->
-									<input placeholder="Repeat Password" name="confirm-password" type="password" autocomplete="off" class="form-control bg-transparent" />
-									<!--end::Repeat Password-->
+								<div class="fv-row mb-3">
+									<!--begin::Password-->
+									<input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
+									<!--end::Password-->
 								</div>
 								<!--end::Input group=-->
 								<!--begin::Submit button-->
 								<div class="d-grid mb-10">
-									<button type="submit" id="kt_sign_up_submit" class="btn btn-primary">
+									<button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
 										<!--begin::Indicator label-->
-										<span class="indicator-label">Sign up</span>
+										<span class="indicator-label">Sign In</span>
 										<!--end::Indicator label-->
 										<!--begin::Indicator progress-->
 										<span class="indicator-progress">Please wait... 
@@ -119,8 +92,8 @@ License: For each use you must have a valid license purchased only from above li
 								</div>
 								<!--end::Submit button-->
 								<!--begin::Sign up-->
-								<div class="text-gray-500 text-center fw-semibold fs-6">Already have an Account? 
-								<a href="authentication/layouts/corporate/sign-in.html" class="link-primary fw-semibold">Sign in</a></div>
+								<div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet? 
+								<a href="{{ route ('sign-up') }}" class="link-primary">Sign up</a></div>
 								<!--end::Sign up-->
 							</form>
 							<!--end::Form-->
@@ -128,10 +101,6 @@ License: For each use you must have a valid license purchased only from above li
 						<!--end::Wrapper-->
 					</div>
 					<!--end::Form-->
-					<!--begin::Footer-->
-					<div class="w-lg-500px d-flex flex-stack px-10 mx-auto">
-					</div>
-					<!--end::Footer-->
 				</div>
 				<!--end::Body-->
 				<!--begin::Aside-->
@@ -139,7 +108,7 @@ License: For each use you must have a valid license purchased only from above li
 					<!--begin::Content-->
 					<div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
 						<!--begin::Logo-->
-						<a href="index.html" class="mb-0 mb-lg-12">
+						<a href="{{ route ('index') }}" class="mb-0 mb-lg-12">
 							<img alt="Logo" src="assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
 						</a>
 						<!--end::Logo-->
@@ -161,7 +130,7 @@ License: For each use you must have a valid license purchased only from above li
 				</div>
 				<!--end::Aside-->
 			</div>
-			<!--end::Authentication - Sign-up-->
+			<!--end::Authentication - Sign-in-->
 		</div>
 		<!--end::Root-->
 		<!--begin::Javascript-->
@@ -171,7 +140,7 @@ License: For each use you must have a valid license purchased only from above li
 		<script src="assets/js/scripts.bundle.js"></script>
 		<!--end::Global Javascript Bundle-->
 		<!--begin::Custom Javascript(used for this page only)-->
-		<script src="assets/js/custom/authentication/sign-up/general.js"></script>
+		<script src="assets/js/custom/authentication/sign-in/general.js"></script>
 		<!--end::Custom Javascript-->
 		<!--end::Javascript-->
 	</body>
